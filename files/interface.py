d10c8ca130dba698272a3caa0874183837eff753
IlyaLeschenko <ilaynext@gmail.com>
2015-04-29 12:43:33 +0300

    text

--- files/interface.py
+++ files/interface.py
@@ -1,9 +1,19 @@
 from tkinter import *
- 
+def printing():
+    print(text)
+
 root = Tk()
-im = PhotoImage(file='lol.gif')
-l = Text(root,height=7,width=7,font='Arial 14',wrap=WORD)
-l = Label(root, image=im)
-l.pack()
+
+im = PhotoImage(file = 'lol.gif')
+back = Label(root, image = im)
+textW = Text(root, height = 10, width = 23, font = 'Arial 14', wrap=WORD)
+
+back.place(x=0, y=0)
+textW.place(x=14, y=150)
+
+but1 = Button(root, text = "print")
+text = textW.get('1.0', END)
+but1.bind("B1", printing)
+but1.pack()
  
 root.mainloop()
